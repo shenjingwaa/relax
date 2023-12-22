c3e1c5d735192f3d88734ae2c57f27d273b1ade4
sjw <3055307597@qq.com>
2023-12-22 14:53:25 +0800

    整理包,代码格式

--- src/main/java/com/relax/relax/common/controller/BaseController.java
+++ src/main/java/com/relax/relax/common/controller/BaseController.java
@@ -1,13 +1,11 @@
 package com.relax.relax.common.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.relax.relax.common.annotation.MappingType;
 import com.relax.relax.common.domain.RelaxResult;
 import com.relax.relax.common.enums.SqlType;
 import com.relax.relax.common.executor.SqlOperationExecutor;
 import com.relax.relax.common.utils.SpringUtil;
-import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.BeanUtils;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -17,61 +15,49 @@ import java.lang.reflect.Field;
 import java.util.Objects;
 
 @SuppressWarnings("unused")
-@Slf4j
-@AllArgsConstructor
 public class BaseController<T> {
 
     private final Class<T> baseEntityClass;
 
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    public BaseController(Class<T> baseEntityClass) {
+        this.baseEntityClass = baseEntityClass;
+    }
+
     @MappingType(RequestMethod.POST)
     @ResponseBody
     public RelaxResult add(@RequestBody T entity,HttpServletRequest request) {
-        try {
-            T instance = baseEntityClass.newInstance();
-            BeanUtils.copyProperties(entity,instance);
-            log.debug("execute add method success,requestBody is {}", entity);
-            return RelaxResult.success(
-                    SpringUtil.getBean(SqlOperationExecutor.class).submit(SqlType.INSERT,request,instance)
-            );
-        } catch (InstantiationException | IllegalAccessException e) {
-            log.error("execute add method for {} fail,requestBody is {}\nand the fail reason is :{}",
-                    this.baseEntityClass.getName(), entity, e.getMessage());
-        }
-        return RelaxResult.fail();
+        T instance = mapper.convertValue(entity, baseEntityClass);
+        return RelaxResult.success(SpringUtil.getBean(SqlOperationExecutor.class).submit(SqlType.INSERT, request, instance));
     }
 
     @MappingType(RequestMethod.POST)
     @ResponseBody
     public RelaxResult update(@RequestBody T entity,HttpServletRequest request) {
-        try {
-            T instance = baseEntityClass.newInstance();
-            BeanUtils.copyProperties(entity,instance);
-            log.debug("execute update method ,requestBody is {}", entity);
-            return RelaxResult.success(
-                    SpringUtil.getBean(SqlOperationExecutor.class).submit(SqlType.UPDATE_BY_ID,request,instance)
-            );
-        } catch (InstantiationException | IllegalAccessException e) {
-            log.error("execute update method for {} fail,requestBody is {}\nand the fail reason is :{}",
-                    this.baseEntityClass.getName(), entity, e.getMessage());
-        }
-        return RelaxResult.fail();
+        T instance = mapper.convertValue(entity, baseEntityClass);
+        return RelaxResult.success(SpringUtil.getBean(SqlOperationExecutor.class).submit(SqlType.UPDATE_BY_ID, request, instance));
     }
 
     @MappingType(RequestMethod.POST)
     @ResponseBody
     public RelaxResult delete(@RequestBody T entity,HttpServletRequest request) {
-        try {
-            T instance = baseEntityClass.newInstance();
-            BeanUtils.copyProperties(entity,instance);
-            log.debug("execute delete method ,requestBody is {}", entity);
-            return RelaxResult.success(
-                    SpringUtil.getBean(SqlOperationExecutor.class).submit(SqlType.DELETE_BY_ID,request,instance)
-            );
-        } catch (InstantiationException | IllegalAccessException e) {
-            log.error("execute delete method for {} fail,requestBody is {}\nand the fail reason is :{}",
-                    this.baseEntityClass.getName(), entity, e.getMessage());
-        }
-        return RelaxResult.fail();
+        T instance = mapper.convertValue(entity, baseEntityClass);
+        return RelaxResult.success(SpringUtil.getBean(SqlOperationExecutor.class).submit(SqlType.DELETE_BY_ID, request, instance));
+    }
+
+    @MappingType(RequestMethod.POST)
+    @ResponseBody
+    public RelaxResult page(@RequestBody T entity, HttpServletRequest request) {
+        T instance = mapper.convertValue(entity, baseEntityClass);
+        return RelaxResult.success(SpringUtil.getBean(SqlOperationExecutor.class).submit(SqlType.SELECT_PAGE, request, instance));
+    }
+
+    @MappingType(RequestMethod.POST)
+    @ResponseBody
+    public RelaxResult list(@RequestBody T entity, HttpServletRequest request) {
+        T instance = mapper.convertValue(entity, baseEntityClass);
+        return RelaxResult.success(SpringUtil.getBean(SqlOperationExecutor.class).submit(SqlType.SELECT_LIST, request, instance));
     }
 
     @MappingType(RequestMethod.GET)
@@ -94,42 +80,7 @@ public class BaseController<T> {
         } else {
             throw new IllegalArgumentException("args format error.");
         }
-        log.debug("execute info method ,RequestParam is id = {}", instance);
-        return RelaxResult.success(
-                SpringUtil.getBean(SqlOperationExecutor.class).submit(SqlType.SELECT_ONE,request,instance)
-        );
-    }
-
-    @MappingType(RequestMethod.POST)
-    @ResponseBody
-    public RelaxResult page(@RequestBody T entity,HttpServletRequest request) {
-        try {
-            T instance = baseEntityClass.newInstance();
-            BeanUtils.copyProperties(entity,instance);
-            return RelaxResult.success(
-                    SpringUtil.getBean(SqlOperationExecutor.class).submit(SqlType.SELECT_PAGE,request,instance)
-            );
-        } catch (InstantiationException | IllegalAccessException e) {
-            log.error("execute page method for {} fail,requestBody is {}\n and the fail reason is :{}",
-                    this.baseEntityClass.getName(), entity, e.getMessage());
-        }
-        return RelaxResult.fail();
-    }
-
-    @MappingType(RequestMethod.POST)
-    @ResponseBody
-    public RelaxResult list(@RequestBody T entity,HttpServletRequest request) {
-        try {
-            T instance = baseEntityClass.newInstance();
-            BeanUtils.copyProperties(entity,instance);
-            return RelaxResult.success(
-                    SpringUtil.getBean(SqlOperationExecutor.class).submit(SqlType.SELECT_LIST,request,instance)
-            );
-        } catch (InstantiationException | IllegalAccessException e) {
-            log.error("execute list method for {} fail,requestBody is {}\nand the fail reason is :{}",
-                    this.baseEntityClass.getName(), entity, e.getMessage());
-        }
-        return RelaxResult.fail();
+        return RelaxResult.success(SpringUtil.getBean(SqlOperationExecutor.class).submit(SqlType.SELECT_ONE, request, instance));
     }
 
 }
